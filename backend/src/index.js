594d79c0280703003fd0263ddfa6bc8e7d1af0b1
Aditya <adityaalba27@gmail.com>
2026-01-25 12:11:46 +0530

    Day-1 complete server and mongodb connected

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./db.js";
+const app = express();
+const PORT = process.env.PORT || 3000
+dotenv.config();
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+app.use(cors());
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
+
+connectDB();
+
+app.listen(PORT,(req,res)=>{
+    console.log(`Server is running on http://localhost:${PORT}/`)
+})
+
