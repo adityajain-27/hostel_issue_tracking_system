66ddd17a021f9e031873c756b29a9ade52cfabd6
Aditya <adityaalba27@gmail.com>
2026-01-26 14:12:31 +0530

    adding autherization for admin and students

--- backend/routes/issueRoutes.js
+++ backend/routes/issueRoutes.js
@@ -5,13 +5,16 @@ import { getPublicIssues } from "../controllers/issueController.js";
 import { updateIssueStatus } from "../controllers/issueController.js";
 import { openIssue } from "../controllers/issueController.js";
 import { resolveIssue } from "../controllers/issueController.js";
+import { authenticate } from "../middlewares/authMiddleware.js";
+import { isAdmin } from "../middlewares/roleMiddleware.js";
+import { isStudent } from "../middlewares/roleMiddleware.js";
 const router =  express.Router();
 
 //get /api/issues/public
 router.get("/public", getPublicIssues);
 
 //post /api/issues
-router.post("/", createissue);
+router.post("/",authenticate,isStudent, createissue);// added autherization for student omly create issue
 
 //get /api/issues
 router.get("/", getAllIssues);
@@ -20,10 +23,10 @@ router.get("/", getAllIssues);
 router.put("/:id/status", updateIssueStatus);
 
 // put /api/issues/:id/open
-router.put("/:id/open", openIssue);
+router.put("/:id/open",authenticate,isAdmin, openIssue); // added authentication and authorization for admin only
 
 // put /api/issues/:id/resolve
-router.put("/:id/resolve", resolveIssue);   
+router.put("/:id/resolve",authenticate,isAdmin, resolveIssue);   //added authentication and authorization for admin only
 
 export default router;
 
